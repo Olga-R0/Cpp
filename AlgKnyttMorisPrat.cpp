6bc595fa7b87009d7562f7a0809b031093e4ff3a
Olga-R0 <166298225+Olga-R0@users.noreply.github.com>
2026-05-10 18:34:53 +0300

    Add files via upload
    Добавлены файлы

---
+++ AlgKnyttMorisPrat.cpp
@@ -0,0 +1,172 @@
+﻿#include <iostream>
+#include <string>
+#include <vector> 
+#include <Windows.h>
+
+using namespace std;
+
+
+vector<int> PrefixFunction(string str_) {
+	int sizeStr = (int)str_.size();
+	vector<int> values(sizeStr, 0);
+
+	for (int i = 1; i < sizeStr; i++)
+		for (int len = 1; len <= i; len++)
+		{	// если префикс длины len равен суффиксу длины len
+			if (str_.substr(0, len) == str_.substr(i - len + 1, len))
+				values[i] = len;
+		}
+	return values;
+}
+vector<int> KMP(string str_, string findStr_) {
+	vector<int> indexes;
+	int count=0;
+	if (findStr_.size() == 0) {
+		indexes.push_back(0);
+		return indexes;
+	}
+	vector<int>function = PrefixFunction(findStr_);
+	
+	int j = 0; //сдвиг по подстроке
+	int i = 0; //сдвиг по строке
+
+	while (i < str_.size()) {
+		if (str_[i] == findStr_[j])
+		{
+			i++; j++;
+			if(j == findStr_.size())
+				indexes.push_back(i - findStr_.size());
+		}
+		else {
+			if (j > 0)
+			{
+				j = function[j - 1]; count++;
+			}
+			else
+				i++;
+		}
+		
+	}
+	cout << endl; cout << "Размер строки " << str_.size() << endl; cout << "Счетчик сдвигов " << str_.size()-count << endl;
+	return indexes;
+	
+}
+// Вывести меню и вернуть номер варианта сортировки
+int menu()
+{
+	// Выбранный вариант меню
+	int variant;
+	cout << "Выберите вариант текста(в котором будет искаться подстрока) и  введите ее номер\n" << endl;
+	cout << "1. Художественный текст. Отрывок из 'Маленького принца'\n"
+		<< "2. Научный текст. Алгоритм КМП\n"
+		<< "3. Свой текст\n"
+		<< "4. выйти\n" << endl;
+	cout << ">>> ";
+	cin >> variant;
+	return variant;
+}
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251); SetConsoleOutputCP(1251);
+	int choise; 
+	string findStr;
+	string str1 = "Алгоритм Кнута — Морриса — Пратта (КМП-алгоритм) — эффективный алгоритм, осуществляющий поиск подстроки в строке. Время работы алгоритма линейно зависит от объёма входных данных, то есть разработать асимптотически более эффективный алгоритм невозможно. Алгоритм был разработан Д. Кнутом и В. Праттом и, независимо от них, Д. Моррисом. Результаты своей работы они опубликовали совместно в 1977 году.";
+	string str2 = "Не скоро я понял, откуда он явился. Маленький принц засыпал меня вопросами, но, когда я спрашивал о чем - нибудь, он будто и не слышал. Лишь понемногу, из случайных, мимоходом оброненных слов мне все открылось. Так, когда он впервые увидел мой самолет(самолет я рисовать не стану, мне все равно не справиться), он спросил :-Что это за штука ? -Это не штука. Это самолет. Мой самолет. Он летает.";
+	string str3;
+	int begin = 0;
+	vector<int> returnValue;
+	cout << "---------Алгоритм Кнута — Морриса — Пратта (КМП-алгоритм)-------" << endl;
+begin:
+	int variant = menu();
+	switch (variant)
+	{
+	case 1:
+		cout << "Ищем в этой подстроке:" << endl;
+		cout << str2 << endl;
+		cout << "Введите какую подстроку вы хотите найти" << endl;
+		cin >> findStr;
+		returnValue = KMP(str2, findStr);
+		cout << endl;
+
+		for (int i = 0; i < returnValue.size(); i++) {
+			cout << "Индекс совпадения:" << returnValue[i] << endl;
+			cout << endl;
+		}
+		if (returnValue.size() == 0)
+			cout << "Нет такой подстроки" << endl;
+			cout << "Запустить другой поиск? Нажмите 0-нет, 1-да " << endl;
+			cin >> choise;
+			switch (choise) {
+			case 1:
+				goto begin; break;
+			case 0:
+				cout << "Выход из программы..." << endl;
+				exit(EXIT_SUCCESS); break;
+			}
+			break;
+	case 2:
+		cout << "Ищем в этой подстроке:" << endl;
+		cout << str1 << endl;
+		cout << "Введите какую подстроку вы хотите найти" << endl;
+		cin >> findStr;
+		returnValue = KMP(str1, findStr);
+		cout << endl;
+
+		for (int i = 0; i < returnValue.size(); i++) {
+			cout << "Индекс совпадения:" << returnValue[i] << endl;
+			cout << endl;
+		}
+		if (returnValue.size() == 0)
+			cout << "Нет такой подстроки" << endl;
+
+		cout << "Запустить другой поиск? Нажмите 0-нет, 1-да " << endl;
+		cin >> choise;
+		switch (choise) {
+		case 1:
+			goto begin; break;
+		case 0:
+			cout << "Выход из программы..." << endl;
+			exit(EXIT_SUCCESS); break;
+		}
+		break;
+	case 3:
+		cout<<"Введите свою строку" << endl;
+		cin >> str3;
+		system("pause");
+		cout << "Введите какую подстроку вы хотите найти" << endl;
+		cin >> findStr;
+
+		
+		returnValue = KMP(str3, findStr);
+		cout << endl;
+
+		for (int i = 0; i < returnValue.size(); i++) {
+			cout << "Индекс совпадения:" << returnValue[i] << endl;
+			cout << endl;
+		}
+		if (returnValue.size() == 0)
+			cout << "Нет такой подстроки" << endl;
+		cout << "Запустить другой поиск? Нажмите 0-нет, 1-да " << endl;
+		cin >> choise;
+		switch (choise) {
+		case 1:
+			goto begin; break;
+		case 0:
+			cout << "Выход из программы..." << endl;
+			exit(EXIT_SUCCESS); break;
+		}
+		break;
+	case 4:
+		cout << "Выход из программы..." << endl;
+		exit(EXIT_SUCCESS);
+		break;
+	default:
+		cerr << "Вы выбрали неверный вариант" << endl;
+		exit(EXIT_FAILURE);
+		}
+
+		return 0;
+	}
+
+
---
+++ Hash.cpp
@@ -0,0 +1,186 @@
+﻿#include <iostream>
+#include < stdio.h >
+
+#include <list>
+using namespace std;
+
+class HashTable
+{
+    int capacity;
+    list<int>* table;
+    
+public:
+    HashTable(int count_);
+    void insertItem(int key_, int data_);
+    void deleteItem(int key_);
+    int size() { return capacity; }
+
+    int checkPrime(int number_)
+    {
+        int i;
+        if (number_ == 1 || number_ == 0)
+        {
+            return 0;
+        }
+        for (i = 2; i < number_ / 2; i++)
+        {
+            if (number_ % i == 0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    int getPrime(int number_)
+    {
+        if (number_ % 2 == 0)
+        {
+            number_++;
+        }
+        while (!checkPrime(number_))
+        {
+            number_ += 2;
+        }
+        return number_;
+    }
+
+    int hashFunction(int key_)
+    {
+        return (key_ % capacity);
+    }
+    void displayHash();
+    int findNode(int data_) {
+        int index = 0;
+        for (int index = 0; index < capacity; index++)
+        {
+            list<int>::iterator it;
+            for (it = table[index].begin();
+                it != table[index].end(); it++)
+            {
+                if (*it == data_)
+                    return index;
+            }
+        }
+        return -1;
+    }
+    void RandElem(int size_)
+    {
+        int size = capacity;
+        
+        for (int index = 0; index < size_; index++)
+        {   
+            int num = rand() % (size );
+            int data = rand();
+            table[num].push_back(data);
+        }
+    }
+};
+HashTable::HashTable(int number_)
+{
+    int size = getPrime(number_);
+    this->capacity = size;
+    table = new list<int>[capacity];
+}
+void HashTable::insertItem(int key_, int data_)
+{
+    int index = hashFunction(key_);
+    table[index].push_back(data_);
+}
+
+void HashTable::deleteItem(int key_)
+{
+    int index = hashFunction(key_);
+
+    list<int>::iterator it;
+    for (it = table[index].begin();
+        it != table[index].end(); it++)
+    {
+        if (*it == key_)
+            break;
+    }
+
+    if (it != table[index].end())
+        table[index].erase(it);
+}
+
+void HashTable::displayHash()
+{
+    for (int i = 0; i < capacity; i++)
+    {
+        cout << "[" << i << "]"<<" : ";
+        for (auto x : table[i])
+            cout <<  x <<"," ;
+        cout << endl;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int choice, key, data, n, loadFactor;
+    int choice2 = 0;
+    
+    cout << "Ведите количество элементов таблицы" << endl;
+    cin >> n;
+    HashTable h(n);
+    do
+    {
+        cout<<"1.Вставить элемент в хэш-таблицу"
+            "\n2.Удалить элемент из хэш-таблицы"
+            "\n3.Узнать размер хэш-таблицы"
+            "\n4.Вывести хэш-таблицу"
+            "\n5.Заполнить таблицу рандомными элементами"
+            "\n6.Найти элемент"
+            "\n\n Пожалуйста, выберите нужный вариант: "<<endl;
+
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Введите ключ" << endl;
+            cin >> key;
+            cout << "Введите данные" << endl;
+            cin >> data;
+            h.insertItem(key,data);
+            
+            break;
+
+        case 2:
+            cout << "Введите ключ, который хотите удалить" << endl;
+            cin >> key;
+            h.deleteItem(key);
+            break;
+
+        case 3:
+
+            n = h.size();
+           
+            cout << "Размер хеш-таблицы " <<n<< endl;
+            break;
+
+        case 4:
+
+            h.displayHash();
+            break;
+
+        case 5:
+            cout << "Введите кол-во элементов, которыми хотите рандомно заполнить таблицу" << endl;
+            cin >> data;
+            h.RandElem(data);
+            break;
+        case 6:
+            cout << "Введите элемент, который хотите найти" << endl;
+            cin >> data;
+           key= h.findNode(data);
+           cout << key<<endl;
+            break;
+        default:
+            cout << "Неверно введены данные" << endl;
+           
+        }
+        cout << "Продолжить? (Нажмите 1, если да): " << endl;
+        cin >> choice2;
+        
+    } while (choice2 == 1);
+
+}
